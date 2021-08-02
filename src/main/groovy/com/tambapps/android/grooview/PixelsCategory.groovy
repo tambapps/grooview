e71c1cb03a065aaf25e03d6c439503789131cac2
Tambapps <tambapps@gmail.com>
2021-08-02 20:26:49 +0200

    made function public

--- src/main/groovy/com/tambapps/android/grooview/PixelsCategory.groovy
+++ src/main/groovy/com/tambapps/android/grooview/PixelsCategory.groovy
@@ -4,36 +4,37 @@ import android.util.TypedValue
 
 class PixelsCategory {
 
+  // should be initialized by the code using this category
   static Object context
 
   static int getPx(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_PX)
+    return convert(number, TypedValue.COMPLEX_UNIT_PX, context)
   }
 
   static int getDp(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_DIP)
+    return convert(number, TypedValue.COMPLEX_UNIT_DIP, context)
   }
 
   static int getDip(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_DIP)
+    return convert(number, TypedValue.COMPLEX_UNIT_DIP, context)
   }
 
   static int getSp(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_SP)
+    return convert(number, TypedValue.COMPLEX_UNIT_SP, context)
   }
 
   static int getPt(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_SP)
+    return convert(number, TypedValue.COMPLEX_UNIT_SP, context)
   }
 
   static int getIn(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_IN)
+    return convert(number, TypedValue.COMPLEX_UNIT_IN, context)
   }
   static int getMm(Number number) {
-    return convert(number, TypedValue.COMPLEX_UNIT_MM)
+    return convert(number, TypedValue.COMPLEX_UNIT_MM, context)
   }
 
-  private static int convert(Number number, int unit) {
+  static int convert(Number number, int unit, Object context) {
     return (int) TypedValue.applyDimension(unit, number.toInteger(), context.resources.displayMetrics)
   }
 
